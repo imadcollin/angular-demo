b7eee38fd997791e280583e06f5fe62ac6fb5533
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-15 22:57:30 +0200

    Add products fetails component

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FilterPipe } from "app/home/product-filter";
+import { ProductsDetailsComponent } from './products-details/products-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FilterPipe
+    FilterPipe,
+    ProductsDetailsComponent
   ],
   imports: [
     BrowserModule,
