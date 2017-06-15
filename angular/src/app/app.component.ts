f78cfefc1bbd2fa9079da2ccdbb3081c400fc222
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-15 16:33:38 +0200

    Add home component contents

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-
+import { HomeComponent } from './home/home.component'; 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: ` <app-home></app-home>     
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
