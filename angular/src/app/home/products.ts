6577e44f7ce796a32fc5867770182189228b76b5
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-15 20:41:43 +0200

    Add product class for futher use

--- angular/src/app/home/products.ts
+++ angular/src/app/home/products.ts
@@ -1,3 +1,4 @@
+import { IProduct } from './products';
 export interface IProduct {
     productId: number;
     productName: string;
@@ -7,4 +8,40 @@ export interface IProduct {
     price: number;
     starRating: number;
     imageUrl: string;
+}
+export class product implements IProduct {
+    constructor(
+        public productId: number,
+        public productName: string,
+        public productCode: string,
+        public releaseDate: string,
+        public description: string,
+        public price: number,
+        public starRating: number,
+        public imageUrl: string,
+    ) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productCode = productCode;
+        this.releaseDate = releaseDate;
+        this.description = description;
+        this.price = price;
+        this.starRating = starRating;
+        this.imageUrl = imageUrl;
+    }
+
+    calaculatePrice(product: number): number {
+        //Calc all pizzes prices
+        if (product != null && product > 140) {
+
+            return product - 15;
+        }
+        else if (product != null && product > 240) {
+            return product - 35;
+
+        }
+        return product;
+    }
+    
+
 }
