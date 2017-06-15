e869412cb826ee4d779f1a08eb1cb8f53bdbb076
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-15 18:21:26 +0200

    Fix image not available

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -26,7 +26,7 @@ products:any[]=[
         "description": "meat and rice",
         "price": 11.55,
         "starRating": 3.7,
-        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRc3wuKVVplx-Gzn1ObB17GV-jPUd4Wpz6wie6gsMUhuptVgEK2"
+        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdYvMliRVZl_vZYsNv6GZc-GW9eZAb4kzxm7JEZ956_THZERa_ixPZ0x0"
     },
      {
         "productId": 5,
