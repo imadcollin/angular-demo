82d01a847caaa1299e6aa965a9da63be123417cc
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-15 22:06:29 +0200

    Use provider intead of hard code data

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { ProductService } from './services/product.service';
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component'; 
 @Component({
   selector: 'app-root',
   template: ` <app-home></app-home>     
   `,
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ProductService]
 })
 export class AppComponent {
   title = 'app works!';
--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './../services/product.service';
 import { IProduct } from './products';
 import { Component, OnInit } from '@angular/core';
 
@@ -8,41 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-listFilter:string='Keb';
-products:IProduct[]=[
-  {
-        "productId": 5,
-        "productName": "Hallon",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "tomate, chicken and sallad",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdYvMliRVZl_vZYsNv6GZc-GW9eZAb4kzxm7JEZ956_THZERa_ixPZ0x0"
-    },
-    {
-        "productId": 8,
-        "productName": "Kebab",
-        "productCode": "TBX-0022",
-        "releaseDate": "May 15, 2016",
-        "description": "meat and rice",
-        "price": 11.55,
-        "starRating": 3.7,
-        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdYvMliRVZl_vZYsNv6GZc-GW9eZAb4kzxm7JEZ956_THZERa_ixPZ0x0"
-    },
-     {
-        "productId": 5,
-        "productName": "Hallon",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "tomate, chicken and sallad",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdYvMliRVZl_vZYsNv6GZc-GW9eZAb4kzxm7JEZ956_THZERa_ixPZ0x0"
-    }];
-  constructor() { }
+listFilter:string='';
+products:IProduct[];
+  constructor(private _productService: ProductService) { 
+  
+  }
 
   ngOnInit() {
+this.products=this._productService.getProducts();
   }
 
 }
