6195eb65f8bca12239f641aac3740b1afc5d930d
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-15 21:31:07 +0200

    Feat some style for search

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-listFilter:string='A';
+listFilter:string='Keb';
 products:IProduct[]=[
   {
         "productId": 5,
