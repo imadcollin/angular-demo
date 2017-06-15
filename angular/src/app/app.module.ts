5b2f06e04fc7b1ef2ac61e6d809ed7743c9ec969
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-15 21:06:56 +0200

    Add required files for filtering

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FilterPipe } from "app/home/product-filter";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+listFilter:string='A';
 products:IProduct[]=[
   {
         "productId": 5,
