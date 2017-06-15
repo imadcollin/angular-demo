2a642d9898a2f21805ab2f4f9ee72f6e1267b7e2
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-15 23:38:16 +0200

    Add router for navigation

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,11 +3,16 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component'; 
 @Component({
   selector: 'app-root',
-  template: ` <app-home></app-home>     
+  template: ` 
+     <router-outlet> </router-outlet>
+
+
   `,
   styleUrls: ['./app.component.css'],
   providers:[ProductService]
 })
 export class AppComponent {
   title = 'app works!';
+  home:string="home";
+  details:string="details";
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FilterPipe } from "app/home/product-filter";
 import { ProductsDetailsComponent } from './products-details/products-details.component';
-
+import{RouterModule} from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +18,14 @@ import { ProductsDetailsComponent } from './products-details/products-details.co
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'home',component:HomeComponent},
+      {path:'details',component:ProductsDetailsComponent},
+      {path:'',redirectTo:'home',pathMatch:'full'},
+      {path:'**',redirectTo:'home',pathMatch:'full'}
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
