a9d3f684190dad5a7d85e3a067b30dd8458ed7ef
Imad Collin <Admin@Imad-MacBook-Pro.local>
2017-06-16 16:28:47 +0200

    Add getProduct by id method

--- angular/src/app/services/product.service.ts
+++ angular/src/app/services/product.service.ts
@@ -1,17 +1,23 @@
 import { IProduct } from './../home/products';
 import { Injectable } from '@angular/core';
-import { Http,Response } from "@angular/http";
+import { Http, Response } from "@angular/http";
 import 'rxjs/add/operator/map';
 import { Observable } from "rxjs/Observable";
 @Injectable()
 export class ProductService {
-    private _dataUrl='/assets/data.json'
-    constructor(private _http: Http){
+    private _dataUrl = '/assets/data.json'
+    constructor(private _http: Http) {
 
 
     }
     getProducts(): Observable<IProduct[]> {
-       return this._http.get(this._dataUrl).map((res:Response)=><IProduct[]> res.json());
-       //Error handling should be implemented 
+        return this._http.get(this._dataUrl).map((res: Response) => <IProduct[]>res.json());
+        //Error handling should be implemented 
+    }
+    //Override getProduct for returning the Object by ID 
+    getProduct(id: number): Observable<IProduct> {
+        console.log("inside the service");
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
     }
 }
